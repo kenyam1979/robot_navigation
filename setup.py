3d6888cac5c92d4fc88e98d35fbcf8360eb38874
kenyam1979 <ken.yamaji@gmail.com>
2025-02-01 17:29:22 +0900

    20250201

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'robot_navigation'
+
+
+data_files = []
+data_files.append(("share/ament_index/resource_index/packages", ["resource/" + package_name]))
+data_files.append(("share/" + package_name, ["package.xml"]))
+
+def package_files(directory, data_files):
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            data_files.append(("share/" + package_name + "/" + path, glob(path + "/**/*.*", recursive=True)))
+    return data_files
+
+data_files = package_files('config/', data_files)
+data_files = package_files('map/', data_files)
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    # data_files=[
+    #     ('share/ament_index/resource_index/packages',
+    #         ['resource/' + package_name]),
+    #     ('share/' + package_name, ['package.xml']),
+    # ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ken',
@@ -23,3 +41,4 @@ setup(
         ],
     },
 )
+
